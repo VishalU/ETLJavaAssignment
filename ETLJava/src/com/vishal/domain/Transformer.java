a6ed97f2d6e48ab2c98e85e92ce43f06b1cedb29
VishalU <vishalau1988@gmail.com>
2021-07-19 18:35:35 +0530

    Testing code push

--- ETLJava/src/com/vishal/domain/Transformer.java
+++ ETLJava/src/com/vishal/domain/Transformer.java
@@ -8,4 +8,12 @@ public interface Transformer {
 	public void load();
 
 }
-Testing testing  mode 
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
