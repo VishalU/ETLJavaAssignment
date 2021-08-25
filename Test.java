5898b98fed0dc2b06e6d4d8d65687944ad82c3bd
VishalU <vishalau1988@gmail.com>
2021-08-25 10:41:10 +0530

    testing

---
+++ Test.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("8");
+		list.add("9");
+		
+		System.out.println("test" + list.get(0)) ;
+	}
+
+}
