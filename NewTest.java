b0797170b0ab6a6e70e183acf42f1d072ab58137
VishalU <vishalau1988@gmail.com>
2022-08-22 22:53:06 +0530

    testing upload

---
+++ NewTest.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NewTest {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("8");
+		list.add("9");
+		system.out.println("Test");
+		System.out.println("test" + list.get(0)) ;
+	}
+
+}
